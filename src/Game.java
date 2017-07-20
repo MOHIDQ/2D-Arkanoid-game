f34e4886faf49f298a6e4aacf56dd42410ea3fc7
Mohid <mohidq99@gmail.com>
2017-07-20 19:14:09 -0400

    Third Commit

--- src/Game.java
+++ src/Game.java
@@ -8,24 +8,30 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 import javax.swing.*;
+import java.awt.Rectangle;
 
 public class Game extends Canvas implements Runnable {
 	public static final int WIDTH = 240;
 	public static final int HEIGHT = WIDTH / 12 * 9;
 	public static final int SCALE = 2;
 	public final String gameTitle = "Arkanoid";
-	
+
 	private boolean running = false;
 	private Thread thread;
-	
-	private BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+
+	private BufferedImage image = new BufferedImage(WIDTH, HEIGHT,
+			BufferedImage.TYPE_INT_RGB);
 	private BufferedImage spriteSheet = null;
-	
+
 	private Player player;
 	private Ball ball;
+
+	private boolean initHit; // checks if ball is still moveable with player
+	private boolean ballHit;
+
+	private Rectangle p; // rectangle shape for player paddle
+	private Rectangle b; // rectangle shape for ball
 	
-	private boolean initHit; //checks if ball is still moveable with player
-		
 	private synchronized void start() {
 		if (running) {
 			return;
@@ -34,7 +40,7 @@ public class Game extends Canvas implements Runnable {
 		thread = new Thread(this);
 		thread.start();
 	}
-	
+
 	private synchronized void stop() {
 		if (!running) {
 			return;
@@ -54,11 +60,11 @@ public class Game extends Canvas implements Runnable {
 		long lastTime = System.nanoTime();
 		final double FPS = 60.0;
 		double ns = 1000000000 / FPS;
-		double delta = 0; //time passed
+		double delta = 0; // time passed
 		int updates = 0;
 		int frames = 0;
 		long timer = System.currentTimeMillis();
-		while(running) {
+		while (running) {
 			long timeNow = System.nanoTime();
 			delta += (timeNow - lastTime) / ns;
 			lastTime = timeNow;
@@ -73,19 +79,23 @@ public class Game extends Canvas implements Runnable {
 				timer += 1000;
 				updates = 0;
 				frames = 0;
-				System.out.println(initHit);
 			}
 		}
 		stop();
-		
+
 	}
-	
+
 	private void tick() {
 		player.tick();
 		ball.tick();
-		
+		wallCollision();
+		if (!initHit) {
+			ballWallCollision();
+			playerCollision();
+		}
+
 	}
-	
+
 	private void render() {
 		BufferStrategy bs = this.getBufferStrategy();
 		if (bs == null) {
@@ -93,58 +103,137 @@ public class Game extends Canvas implements Runnable {
 			return;
 		}
 		Graphics g = bs.getDrawGraphics();
-		g.drawImage(image, 0, 0, getWidth(), getHeight(), this); 
+		g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
 		ball.render(g);
 		player.render(g);
-		
-		
-		
+
 		g.dispose();
 		bs.show();
 	}
-	
+
 	public void initial() {
 		BufferedImageLoader loader = new BufferedImageLoader();
 		try {
 			spriteSheet = loader.loadImage("/spritesheet.png");
-		}catch (IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		addKeyListener(new UserInput(this));
-		player = new Player(200, 300, this);
+		player = new Player(200, 300, this, 100, 20);
 		ball = new Ball(this, player);
 		initHit = true;
+		ballHit = true;
 	}
-	
-	public void keyPressed (KeyEvent e) { 
+
+	public void keyPressed(KeyEvent e) {
 		int key = e.getKeyCode();
+		// if right key is hit change speed of paddle
 		if (key == KeyEvent.VK_RIGHT) {
-			player.setXSpeed(1);
+			player.setXSpeed(3);
 			if (initHit == true)
 				ball.setXSpeed(player.getXSpeed());
 		}
+		// if left key is hit change speed of paddle
 		else if (key == KeyEvent.VK_LEFT) {
-			player.setXSpeed(-1);
+			player.setXSpeed(-3);
 			if (initHit == true)
 				ball.setXSpeed(player.getXSpeed());
 		}
+		// if space bar is hit, release ball from paddle
 		else if (key == KeyEvent.VK_SPACE) {
+			if (initHit) {
+				ball.setXSpeed(2);
+				ball.setYSpeed(-2);
+			}
 			initHit = false;
-			ball.setXSpeed(1);
-			ball.setYSpeed(-1);
 		}
 	}
-	
+
 	public void keyReleased(KeyEvent e) {
-		
+		int key = e.getKeyCode();
+
+		if (key == KeyEvent.VK_RIGHT) {
+			player.setXSpeed(0);
+			if (initHit == true)
+				ball.setXSpeed(player.getXSpeed());
+		}
+		// if left key is hit change speed of paddle
+		else if (key == KeyEvent.VK_LEFT) {
+			player.setXSpeed(0);
+			if (initHit == true)
+				ball.setXSpeed(player.getXSpeed());
+		}
 	}
-	
+
+	// method to check collisions between of player paddle and wall
+	public void wallCollision() {
+		if (player.getXPos() < 0) {
+			player.setXSpeed(2);
+			if (initHit)
+				ball.setXSpeed(player.getXSpeed());
+		} else if (player.getxPos() > 455) {
+			player.setXSpeed(-2);
+			if (initHit)
+				ball.setXSpeed(player.getXSpeed());
+		}
+	}
+
+	// method to check collisions between wall and ball
+	public void ballWallCollision() {
+		// checks if ball hits left side of frame
+		if (ball.getxPos() < -35) {
+			ball.setXSpeed(-ball.getxSpeed());
+			ballHit = false;
+		}
+		// checks if ball hits right side of frame
+		else if (ball.getxPos() > 425) {
+			ball.setxSpeed(-ball.getxSpeed());
+			ballHit = false;
+		}
+		// checks if ball hits top side of frame
+		else if (ball.getyPos() < 15) {
+			ball.setySpeed(-ball.getySpeed());
+			ballHit = false;
+		}
+	}
+
+	// method to check collision between player paddle and wall
+	public void playerCollision() {
+		p = new Rectangle((int) player.getxPos(), (int) player.getyPos(), player.getWidth(), player.getHeight());
+		b = new Rectangle((int) ball.getxPos(), (int) ball.getyPos(), ball
+				.getBall().getWidth(), ball.getBall().getHeight() - 20);
+		if (p.intersects(b)) {
+			if (!ballHit) {
+				if (ball.getxSpeed() > 0 && ball.getxPos() < player.getxPos()) {
+					ball.setySpeed(-ball.getySpeed());
+					ball.setxSpeed(-ball.getxSpeed());
+					System.out.println("1");
+				}
+				else if (ball.getxSpeed() < 0 && ball.getxPos() < player.getxPos()) {
+					ball.setySpeed(-ball.getySpeed());
+					System.out.println("2");
+				}
+				else if (ball.getxSpeed() > 0 && ball.getxPos() > player.getxPos()) {
+					ball.setySpeed(-ball.getySpeed());
+					System.out.println("3");
+				}
+				else if (ball.getxSpeed() < 0 && ball.getxPos() > player.getxPos()) {
+					ball.setySpeed(-ball.getySpeed());
+					System.out.println("4");
+				}
+				else
+					ball.setySpeed(-ball.getySpeed());
+			}
+		}
+
+	}
+
 	public static void main(String args[]) {
 		Game game = new Game();
 		game.setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
 		game.setMaximumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
 		game.setMinimumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
-		
+
 		JFrame frame = new JFrame(game.gameTitle);
 		frame.add(game);
 		frame.pack();
@@ -152,16 +241,12 @@ public class Game extends Canvas implements Runnable {
 		frame.setResizable(false);
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
-		
+
 		game.start();
 	}
-	
+
 	public BufferedImage getSpriteSheet() {
 		return spriteSheet;
 	}
-	
-	
-	
-	
 
 }
--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,6 @@
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 
 
@@ -7,12 +9,18 @@ public class Player {
 	private double yPos;
 	private int xSpeed;
 	private int ySpeed;
-	private BufferedImage player;
-	public Player(double xPos, double yPos, Game game) {
+	private int width;
+	private int height;
+	//private BufferedImage player;
+	private Rectangle player;
+	public Player(double xPos, double yPos, Game game, int width, int height) {
 		this.xPos = xPos;
 		this.yPos = yPos;
+		this.width = width;
+		this.height = height;
 		SpriteSheet ss = new SpriteSheet(game.getSpriteSheet());
-		player = ss.grabImage(2, 1, 32, 32);
+		//player = ss.grabImage(2, 1, 32, 32);
+		player = new Rectangle((int)xPos, (int)yPos, width, height);
 		
 	}
 	
@@ -21,7 +29,9 @@ public class Player {
 	}
 	
 	public void render(Graphics g) {
-		g.drawImage(player, (int)xPos, (int)yPos, null);
+		g.setColor(Color.white);
+		g.drawRect((int)xPos, (int)yPos, width, height);
+		
 	}
 	
 	public double getXPos() {
@@ -43,6 +53,39 @@ public class Player {
 	public void setXSpeed(int xSpeed) {
 		this.xSpeed = xSpeed;
 	}
+
+	public double getxPos() {
+		return xPos;
+	}
+
+	//public BufferedImage getPlayer() {
+		//return player;
+	//}
+
+	public void setPlayer(BufferedImage player) {
+		//this.player = player;
+	}
+
+	public void setxPos(double xPos) {
+		this.xPos = xPos;
+	}
+
+	public double getyPos() {
+		return yPos;
+	}
+
+	public void setyPos(double yPos) {
+		this.yPos = yPos;
+	}
+	
+	public int getWidth() {
+		return this.width;
+	}
+	
+	public int getHeight() {
+		return this.height;
+	}
+	
 	
 	
 
